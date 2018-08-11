79219449cfebdb20bae551c98880a68218f0fb97
grenos <Vasilis.green@gmail.com>
2018-08-11 20:28:08 +0300

    updates
    updates webpack.config for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
 const webpack = require('webpack');
 const myEnv = require('dotenv').config();
 const path = require('path');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/App.js', // here run path of src/App.js
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/, //support of css and scss
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
+module.exports = env => {
+  const isProduction = env === 'production';
+  //const CSSextract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/App.js', // here run path of src/App.js
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/, //support of css and scss
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true // tells server we use client router and should always serve index.html
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        API_KEY: JSON.stringify(myEnv.parsed.API_KEY)
+      }),
+      //CSSextract
     ]
-  },
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true // tells server we use client router and should always serve index.html
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      API_KEY: JSON.stringify(myEnv.parsed.API_KEY)
-    })
-  ]
+  };
 };
